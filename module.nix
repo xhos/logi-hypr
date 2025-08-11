b63475506772cb8dd946a329895f762c032ec9bc
xhos <60789741+xhos@users.noreply.github.com>
2025-08-11 08:32:21 -0400

    fix nix module

--- module.nix
+++ module.nix
@@ -13,88 +13,137 @@
     cargoLock.lockFile = ./Cargo.lock;
 
     nativeBuildInputs = [pkgs.pkg-config];
-    buildInputs = with pkgs; [wayland wayland-protocols libxkbcommon];
+    buildInputs = [pkgs.systemd];
   };
 
   configFile = pkgs.writeText "logi-hypr.toml" ''
     [gesture]
-    threshold = ${toString cfg.threshold}
+    tap_timeout_ms = ${toString cfg.gesture.tapTimeoutMs}
+    movement_threshold = ${toString cfg.gesture.movementThreshold}
 
     [gesture.commands]
-    tap = "${cfg.commands.tap}"
-    up = "${cfg.commands.up}"
-    down = "${cfg.commands.down}"
-    left = "${cfg.commands.left}"
-    right = "${cfg.commands.right}"
+    tap = "${cfg.gesture.commands.tap}"
+    swipe_left = "${cfg.gesture.commands.swipeLeft}"
+    swipe_right = "${cfg.gesture.commands.swipeRight}"
+    swipe_up = "${cfg.gesture.commands.swipeUp}"
+    swipe_down = "${cfg.gesture.commands.swipeDown}"
 
     [scroll]
-    default_action = "native"
+    focus_poll_ms = ${toString cfg.scroll.focusPollMs}
 
-    [scroll.applications]
-    ${lib.concatStringsSep "\n" (lib.mapAttrsToList (app: action: "${app} = \"${action}\"") cfg.scrollApps)}
+    ${lib.concatMapStrings (rule: ''
+        [[scroll.rules]]
+        window_class_regex = "${rule.windowClassRegex}"
+        scroll_right_commands = [${lib.concatMapStringsSep ", " (cmd: "\"${cmd}\"") rule.scrollRightCommands}]
+        scroll_left_commands = [${lib.concatMapStringsSep ", " (cmd: "\"${cmd}\"") rule.scrollLeftCommands}]
+
+      '')
+      cfg.scroll.rules}
   '';
 in {
   options.programs.logi-hypr = {
     enable = lib.mkEnableOption "logitech gesture handler";
 
-    threshold = lib.mkOption {
-      type = lib.types.float;
-      default = 50.0;
-      description = "movement threshold for gestures";
+    enableService = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "whether to enable the systemd user service";
     };
 
-    commands = {
-      tap = lib.mkOption {
-        type = lib.types.str;
-        default = "hyprctl dispatch exec wofi";
-        description = "command for tap gesture";
+    gesture = {
+      tapTimeoutMs = lib.mkOption {
+        type = lib.types.int;
+        default = 200;
+        description = "tap timeout in milliseconds";
       };
 
-      up = lib.mkOption {
-        type = lib.types.str;
-        default = "hyprctl dispatch workspace +1";
-        description = "command for up gesture";
+      movementThreshold = lib.mkOption {
+        type = lib.types.int;
+        default = 100;
+        description = "movement threshold in pixels";
       };
 
-      down = lib.mkOption {
-        type = lib.types.str;
-        default = "hyprctl dispatch workspace -1";
-        description = "command for down gesture";
-      };
+      commands = {
+        tap = lib.mkOption {
+          type = lib.types.str;
+          default = "hyprctl dispatch togglespecialworkspace";
+        };
 
-      left = lib.mkOption {
-        type = lib.types.str;
-        default = "hyprctl dispatch movetoworkspace -1";
-        description = "command for left gesture";
-      };
+        swipeLeft = lib.mkOption {
+          type = lib.types.str;
+          default = "playerctl --player=spotify previous";
+        };
 
-      right = lib.mkOption {
-        type = lib.types.str;
-        default = "hyprctl dispatch movetoworkspace +1";
-        description = "command for right gesture";
+        swipeRight = lib.mkOption {
+          type = lib.types.str;
+          default = "playerctl --player=spotify next";
+        };
+
+        swipeUp = lib.mkOption {
+          type = lib.types.str;
+          default = "hyprctl dispatch workspace m-1";
+        };
+
+        swipeDown = lib.mkOption {
+          type = lib.types.str;
+          default = "hyprctl dispatch workspace m+1";
+        };
       };
     };
 
-    scrollApps = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      default = {
-        spotify = "volume";
-        gimp = "zoom";
+    scroll = {
+      focusPollMs = lib.mkOption {
+        type = lib.types.int;
+        default = 200;
+        description = "focus polling interval in milliseconds";
       };
-      description = "per-app scroll behaviors";
+
+      rules = lib.mkOption {
+        type = lib.types.listOf (lib.types.submodule {
+          options = {
+            windowClassRegex = lib.mkOption {
+              type = lib.types.str;
+              description = "window class regex pattern";
+            };
+
+            scrollRightCommands = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [];
+            };
+
+            scrollLeftCommands = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [];
+            };
+          };
+        });
+        default = [
+          {
+            windowClassRegex = "Spotify";
+            scrollRightCommands = ["wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"];
+            scrollLeftCommands = ["wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"];
+          }
+          {
+            windowClassRegex = "firefox|chrome|chromium|zen";
+            scrollRightCommands = ["wtype -M ctrl -k Tab"];
+            scrollLeftCommands = ["wtype -M ctrl -M shift -k Tab"];
+          }
+        ];
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [logi-hypr];
 
-    systemd.user.services.logi-hypr = {
+    systemd.user.services.logi-hypr = lib.mkIf cfg.enableService {
       description = "logitech gesture handler";
       wantedBy = ["graphical-session.target"];
 
       serviceConfig = {
         ExecStart = "${logi-hypr}/bin/logi-hypr --config ${configFile}";
         Restart = "on-failure";
+        SupplementaryGroups = ["input"];
       };
     };
 
