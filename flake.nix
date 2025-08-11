d8dce5d6afb001a261c530ce6d5cf23930eba84e
xhos <60789741+xhos@users.noreply.github.com>
2025-08-11 08:41:01 -0400

    fix nix flake

--- flake.nix
+++ flake.nix
@@ -4,18 +4,51 @@
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f:
+      builtins.listToAttrs (map (system: {
+          name = system;
+          value = f system;
+        })
+        systems);
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        cargo
-        rustc
-        pkg-config
-      ];
-    };
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "logi-gesture";
+        version = "0.1.0";
+        src = self;
+        cargoLock.lockFile = ./Cargo.lock;
+        nativeBuildInputs = [pkgs.pkg-config];
+      };
+    });
 
-    nixosModules.default = import ./module.nix;
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [cargo rustc pkg-config];
+      };
+    });
+
+    nixosModules.default = {
+      lib,
+      pkgs,
+      config,
+      ...
+    }: let
+      logiGesturePkg = self.packages.${pkgs.system}.default;
+    in
+      (import ./module.nix) {
+        inherit lib pkgs config;
+        logiGesturePkg = logiGesturePkg;
+      };
+
+    overlays.default = final: prev: {
+      logi-gesture = self.packages.${final.system}.default;
+    };
   };
 }
